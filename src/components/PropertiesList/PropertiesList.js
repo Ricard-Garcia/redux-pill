28c986577c6666615e5ec0a9ccf33e2c25b43664
Ricard Garcia <imricardgarcia@gmail.com>
2021-09-07 15:02:48 +0200

    fix: solved key prop problem in PropertyCard

--- src/components/PropertiesList/PropertiesList.js
+++ src/components/PropertiesList/PropertiesList.js
@@ -3,15 +3,12 @@ import React from "react";
 import PropertyCard from "../PropertyCard";
 
 function PropertiesList({ foundProperties }) {
-  // Redux properties
-  // const properties = [{ name: "p1" }, { name: "p2" }];
-
   console.log(foundProperties, "Found properties!");
 
   return (
     <div className="d-flex flex-column">
       {foundProperties.map((property) => {
-        return <PropertyCard foundProperty={property} />;
+        return <PropertyCard key={property.id} foundProperty={property} />;
       })}
     </div>
   );
--- src/components/PropertyCard/PropertyCard.js
+++ src/components/PropertyCard/PropertyCard.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function PropertyCard({ foundProperty }) {
   return (
-    <div key={foundProperty.id} className="d-flex flex-column">
+    <div className="d-flex flex-column">
       <p>{foundProperty.id}</p>
       <p>{foundProperty.city}</p>
     </div>
